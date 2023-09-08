930dd1505ad8d0e21984a358e4761c8e2f19ac0b
Brent Sullivan <brntsllvn@gmail.com>
2023-09-08 18:11:55 +0000

    fix tables

--- src/robbie_ray.py
+++ src/robbie_ray.py
@@ -37,10 +37,9 @@ freq_table = freq_table[[('count', 2021), ('count', 2022), ('count', 'diff'), ('
 ax1.table(cellText=freq_table.values, colLabels=freq_table.columns, rowLabels=freq_table.index, loc='center')
 plt.savefig('FrequencyTable.png')
 
-# Subsequent Pages: Each feature row
 for feature in features:
-    fig = plt.figure(figsize=(11, 8.5))
-    gs = gridspec.GridSpec(1, len(pitch_types), hspace=1, wspace=1)
+    fig = plt.figure(figsize=(16, 9))
+    gs = gridspec.GridSpec(2, len(pitch_types), height_ratios=[3, 1], hspace=1, wspace=1)
 
     for j, pitch in enumerate(pitch_types):
         ax = plt.subplot(gs[0, j])
@@ -49,23 +48,25 @@ for feature in features:
         subset_data = ray_stats[ray_stats['pitch_type'] == pitch]
         subset_data.boxplot(column=feature, by='year', ax=ax)
 
-        # Labels and Titles
-        ax.set_title(f"{pitch}", pad=48)
-        ax.set_xlabel("")
-        ax.set_ylabel("")
-
-        # Row Title for First Column
-        if j == 0:
-            ax.annotate(f"{feature}", xy=(0, 1.4), xycoords='axes fraction', fontsize=48, ha="center", va="center")
-
-        # Stats Table Below Plot
-        stats_2021 = subset_data[subset_data['year'] == 2021][feature].describe()
-        stats_2022 = subset_data[subset_data['year'] == 2022][feature].describe()
+        # Table with statistical summary
+        ax_table = plt.subplot(gs[1, j])
+        stats_2021 = subset_data[subset_data['year'] == 2021][feature].describe().round(0)
+        stats_2022 = subset_data[subset_data['year'] == 2022][feature].describe().round(0)
         stats_diff = stats_2022 - stats_2021
         table_data = pd.DataFrame({'2021': stats_2021, '2022': stats_2022, 'Diff': stats_diff})
         table_data = table_data.loc[['mean', '50%', 'min', 'max', 'std']].fillna(0)
-        ax.table(cellText=table_data.values, colLabels=table_data.columns,
-                 rowLabels=table_data.index, loc='bottom', bbox=[0, -0.6, 1, 0.3])
 
-    plt.subplots_adjust(hspace=1, wspace=1)
+        ax_table.axis('tight')
+        ax_table.axis('off')
+        ax_table.table(cellText=table_data.values, colLabels=table_data.columns,
+                       rowLabels=table_data.index, loc='center', colWidths=[0.2]*3, fontsize=12)
+
+        ax.set_title(f"{pitch}")
+        ax.set_xlabel("")
+        ax.set_ylabel("")
+
+        if j == 0:
+            ax.annotate(f"{feature}", xy=(0, 1.5), xycoords='axes fraction', fontsize=20, ha="center", va="center")
+
+    plt.subplots_adjust(bottom=0.3)
     plt.savefig(f"{feature}_Metrics.png")
